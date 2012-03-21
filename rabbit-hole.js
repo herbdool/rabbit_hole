e04f628306211b79e50623f7ec58bfbabd560a49
cyberpunk <cyberpunk@22093.no-reply.drupal.org>
2012-03-21 10:57:44 +0100

    Issue #1412786 doublejosh: Created a 6.x version.

--- rabbit-hole.js
+++ rabbit-hole.js
@@ -1,39 +1,16 @@
-(function($) {
-
-Drupal.behaviors.rabbitHole = {
-  attach: function (context, settings) {
-
-    // Set the summary for the settings form.
-    $('fieldset.rabbit-hole-settings-form').drupalSetSummary(function() {
-      var $rabbitHoleAction = $('#edit-rabbit-hole-action input:checked');
-      
-      // Get the label of the selected action.
-      var summary = $('label[for=' + $rabbitHoleAction.attr('id') + ']').text();
-      
-      // If redirect is selected, add the url to the summary.
-      if ($rabbitHoleAction.val() == settings.rabbitHole.redirectValue) {
-        summary += ' (' + $('#edit-rabbit-hole-redirect').val() + ')';
-      }
-      
-      return Drupal.checkPlain(summary);
-    });
-  
-    // Only show the redirect options if the user has selected redirect as the
-    // behavior.
-    var $redirectFieldset = $('fieldset.rabbit-hole-redirect-options');
-    if ($('#edit-rabbit-hole input[name=rabbit_hole_action]:checked').val() != settings.rabbitHole.redirectValue) {
+Drupal.behaviors.rabbitHole = function (context) {  
+  // Only show the redirect options if the user has selected redirect as the
+  // behavior.
+  var $redirectFieldset = $('fieldset.rabbit-hole-redirect-options');
+  if ($('.rabbit-hole-settings-form input[name=rabbit_hole_action]:checked').val() != Drupal.settings.rabbitHole.redirectValue) {
+    $redirectFieldset.css('display', 'none');
+  }
+  $('.rabbit-hole-settings-form input[name=rabbit_hole_action]').change(function() {
+    if ($(this).val() == Drupal.settings.rabbitHole.redirectValue) {
+      $redirectFieldset.show();
+    }
+    else {
       $redirectFieldset.hide();
     }
-    $('#edit-rabbit-hole input[name=rabbit_hole_action]').change(function() {
-      if ($(this).val() == settings.rabbitHole.redirectValue) {
-        $redirectFieldset.show();
-      }
-      else {
-        $redirectFieldset.hide();
-      }
-    });
-  
-  }
+  });
 }
-
-})(jQuery);
