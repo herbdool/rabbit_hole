8fb094af36a583740b6d8e7cdde44b90184ddbf2
esclapes <esclapes@104740.no-reply.drupal.org>
2014-07-10 22:35:03 +0200

    Issue #2276289 by esclapes: Fixed js not working with vertical tabs.

--- rabbit-hole.js
+++ rabbit-hole.js
@@ -2,10 +2,10 @@ Drupal.behaviors.rabbitHole = function (context) {
   // Only show the redirect options if the user has selected redirect as the
   // behavior.
   var $redirectFieldset = $('fieldset.rabbit-hole-redirect-options');
-  if ($('.rabbit-hole-settings-form input[name=rabbit_hole_action]:checked').val() != Drupal.settings.rabbitHole.redirectValue) {
+  if ($('input[name=rabbit_hole_action]:checked').val() != Drupal.settings.rabbitHole.redirectValue) {
     $redirectFieldset.css('display', 'none');
   }
-  $('.rabbit-hole-settings-form input[name=rabbit_hole_action]').change(function() {
+  $('input[name=rabbit_hole_action]').change(function() {
     if ($(this).val() == Drupal.settings.rabbitHole.redirectValue) {
       $redirectFieldset.show();
     }
